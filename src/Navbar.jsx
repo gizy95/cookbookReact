2300710bf9dcdf0879cc675a2565b38d520c181b
LenaBultmann <lena.bultmann@gmx.de>
2024-01-09 11:57:49 +0100

    adding basic structure for navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( <>
+    <div className="navbar">
+        <div>Cookbook</div>
+        <ul>
+            <li>Salads</li>
+            <li>Main Dishes</li>
+            <li>Desserts</li>
+        </ul>
+    </div>
+    </> );
+}
+ 
+export default Navbar;
